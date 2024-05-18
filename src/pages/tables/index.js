150460de2f28a6525c1b7de6d4fca7f097995887
phanhuuviet <86942377+phanhuuviet@users.noreply.github.com>
2024-05-18 16:54:58 +0700

    update/custom-table

--- src/pages/tables/index.js
+++ src/pages/tables/index.js
@@ -4,6 +4,7 @@ import Link from '@mui/material/Link'
 import Card from '@mui/material/Card'
 import Typography from '@mui/material/Typography'
 import CardHeader from '@mui/material/CardHeader'
+import VisibilityIcon from '@mui/icons-material/Visibility'
 
 // ** Demo Components Imports
 import TableBasic from 'src/views/tables/TableBasic'
@@ -12,8 +13,88 @@ import TableSpanning from 'src/views/tables/TableSpanning'
 import TableCustomized from 'src/views/tables/TableCustomized'
 import TableCollapsible from 'src/views/tables/TableCollapsible'
 import TableStickyHeader from 'src/views/tables/TableStickyHeader'
+import CustomTable from 'src/views/tables/CustomTable'
+import { IconButton } from '@mui/material'
 
 const MUITable = () => {
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+      field: 'firstName',
+      headerName: 'First name',
+      width: 150,
+      renderCell: params => {
+        console.log(params)
+
+        return params?.row?.user?.name
+      }
+    },
+    {
+      field: 'lastName',
+      headerName: 'Last name',
+      width: 150
+    },
+    {
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+      width: 110
+    },
+    {
+      field: 'fullName',
+      headerName: 'Full name',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 160
+    },
+    {
+      field: 'Action',
+      headerName: 'Action',
+      sortable: false,
+      width: 200,
+      headerAlign: 'center',
+      renderCell: params => {
+        return (
+          <>
+            <IconButton onClick={() => console.log(params.row)}>
+              <VisibilityIcon />
+            </IconButton>
+            <IconButton onClick={() => console.log(123)}>
+              <VisibilityIcon />
+            </IconButton>
+            <IconButton onClick={() => console.log(123)}>
+              <VisibilityIcon />
+            </IconButton>
+            <IconButton onClick={() => console.log(123)}>
+              <VisibilityIcon />
+            </IconButton>
+          </>
+        )
+      }
+    }
+  ]
+
+  const rows = [
+    {
+      id: 1,
+      lastName: 'Snow',
+      firstName: 'Jon',
+      age: 14,
+      user: {
+        name: '1',
+        age: '2'
+      }
+    },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 }
+  ]
+
   return (
     <Grid container spacing={6}>
       <Grid item xs={12}>
@@ -23,6 +104,12 @@ const MUITable = () => {
           </Link>
         </Typography>
         <Typography variant='body2'>Tables display sets of data. They can be fully customized</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Card>
+          <CardHeader title='Custom Table' titleTypographyProps={{ variant: 'h6' }} />
+          <CustomTable rows={rows} columns={columns} defaultPageSize={5} isViewFilter={true} />
+        </Card>
       </Grid>
       <Grid item xs={12}>
         <Card>
