0820fe39d3b8977ff11663641f2665d4d7a788e7
phanhuuviet <86942377+phanhuuviet@users.noreply.github.com>
2024-05-22 23:59:30 +0700

    update/middleware

---
+++ src/pages/user-management/[id].js
@@ -0,0 +1,98 @@
+// ** React Imports
+import { useEffect, useState } from 'react'
+
+// ** MUI Imports
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import TabList from '@mui/lab/TabList'
+import TabPanel from '@mui/lab/TabPanel'
+import TabContext from '@mui/lab/TabContext'
+import { styled } from '@mui/material/styles'
+import MuiTab from '@mui/material/Tab'
+
+// ** Icons Imports
+import AccountOutline from 'mdi-material-ui/AccountOutline'
+import LockOpenOutline from 'mdi-material-ui/LockOpenOutline'
+import InformationOutline from 'mdi-material-ui/InformationOutline'
+
+// ** Demo Tabs Imports
+import TabInfo from 'src/views/account-settings/TabInfo'
+import TabAccount from 'src/views/account-settings/TabAccount'
+import TabSecurity from 'src/views/account-settings/TabSecurity'
+
+// ** Third Party Styles Imports
+import 'react-datepicker/dist/react-datepicker.css'
+import { useRouter } from 'next/router'
+import { date } from 'yup'
+
+const Tab = styled(MuiTab)(({ theme }) => ({
+  [theme.breakpoints.down('md')]: {
+    minWidth: 100
+  },
+  [theme.breakpoints.down('sm')]: {
+    minWidth: 67
+  }
+}))
+
+const TabName = styled('span')(({ theme }) => ({
+  lineHeight: 1.71,
+  fontSize: '0.875rem',
+  marginLeft: theme.spacing(2.4),
+  [theme.breakpoints.down('md')]: {
+    display: 'none'
+  }
+}))
+
+const AccountSettings = () => {
+  const router = useRouter()
+
+  const mockData = {
+    address: '1234 Street Name, City Name',
+    dateOfBirth: new Date(),
+    email: 'nguyenvana@gmail.com',
+    firstName: 'Nguyen',
+    lastName: 'Van A',
+    gender: true,
+    phoneNumber: '123456789',
+    role: 'renter'
+  }
+
+  useEffect(() => {
+    console.log('call api here')
+  }, [router?.query?.id])
+
+  // ** State
+  const [value, setValue] = useState('account')
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue)
+  }
+
+  return (
+    <Card>
+      <TabContext value={value}>
+        <TabList
+          onChange={handleChange}
+          aria-label='account-settings tabs'
+          sx={{ borderBottom: theme => `1px solid ${theme.palette.divider}` }}
+        >
+          <Tab
+            value='account'
+            label={
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <AccountOutline />
+                <TabName>Account</TabName>
+              </Box>
+            }
+          />
+        </TabList>
+
+        <TabPanel sx={{ p: 0 }} value='account'>
+          <TabAccount />
+        </TabPanel>
+      </TabContext>
+    </Card>
+  )
+}
+
+export default AccountSettings
